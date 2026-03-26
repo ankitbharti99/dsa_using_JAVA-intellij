29f3facd0b995b0a517b639cdff95a20c8739c4b
Ankit_Bharti <152873570+ankitbharti99@users.noreply.github.com>
2026-03-26 23:57:36 +0530

    optimised

--- src/com/Recursion/Array/SortedorNot.java
+++ src/com/Recursion/Array/SortedorNot.java
@@ -3,15 +3,15 @@ package com.Recursion.Array;
 public class SortedorNot {
     static void main() {
         int []arr = {1,3,4,7,9};
-        System.out.println(sorted(arr,0,1));
+        System.out.println(sorted(arr,0));
     }
 
-    static boolean sorted(int[] arr, int p1, int p2) {
-        if(p2 == arr.length-1){
+    static boolean sorted(int[] arr, int p1) {
+        if(p1 == arr.length-1){
             return true;
         }
-        if(arr[p1] < arr[p2]) {
-            return sorted(arr, ++p1, ++p2);
+        if(arr[p1] < arr[p1+1]) {
+            return sorted(arr, p1+1);
         }
         return false;
     }

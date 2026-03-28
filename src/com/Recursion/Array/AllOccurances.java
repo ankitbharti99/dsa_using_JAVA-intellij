74606e0ddbaf3030292b15c3d4d98dbf414474c1
Ankit_Bharti <152873570+ankitbharti99@users.noreply.github.com>
2026-03-28 19:40:48 +0530

    commit

---
+++ src/com/Recursion/Array/AllOccurances.java
@@ -0,0 +1,28 @@
+package com.Recursion.Array;
+
+import java.util.ArrayList;
+
+public class AllOccurances {
+    static void main() {
+        int[] arr = {1,4,1,5,3,7,7,4,1};
+        FindAllOccurance(arr,1,0);
+        System.out.println(list);
+
+        //2nd way, return Arraylist, no need to declare Arraylist outside the function.
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(FindAllOccurance2(arr,1,0,list));
+    }
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void FindAllOccurance(int[] arr, int target, int index) {
+        if(index > arr.length-1)  return;
+        else if(arr[index] == target) list.add(index);
+        FindAllOccurance(arr,target,index+1);
+    }
+
+    static ArrayList<Integer> FindAllOccurance2(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if(index > arr.length-1)  return list;
+        else if(arr[index] == target) list.add(index);
+        return FindAllOccurance2(arr,target,index+1, list);
+    }
+}

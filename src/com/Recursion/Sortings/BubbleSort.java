0a9914d2453b9dae81efa547eeba8394e2f23429
Ankit_Bharti <152873570+ankitbharti99@users.noreply.github.com>
2026-04-13 13:21:22 +0530

    commit

--- src/com/Recursion/Sortings/BubbleSort.java
+++ src/com/Recursion/Sortings/BubbleSort.java
@@ -2,6 +2,27 @@ package com.Recursion.Sortings;
 
 public class BubbleSort {
     static void main() {
+        int[] arr = {2,6,1,4,3,7,5};
+        BSort(arr,arr.length-1,0);
+    }
 
+    static void BSort(int[] arr,int row, int col) {
+        if(row==0) {
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            return;
+        }
+
+        if(col<row) {
+            if(arr[col] > arr[col+1]) {
+                int temp = arr[col];
+                arr[col] = arr[col+1];
+                arr[col+1] = temp;
+            }
+            BSort(arr,row,col+1);
+        } else{
+            BSort(arr,row-1,0);
+        }
     }
 }

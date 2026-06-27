00c6393ba62657a372c3a3ecccd1ed2c7dbec5d5
Ankit_Bharti <152873570+ankitbharti99@users.noreply.github.com>
2026-06-28 01:08:06 +0530

    commit

---
+++ src/com/OOP/abstractDemo/Parent.java
@@ -0,0 +1,15 @@
+package com.OOP.abstractDemo;
+
+public abstract class Parent {
+
+    String ageGroup;
+
+    //We can create constructor of Parent Class and call it through "super" class from the child class
+    public Parent(String ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+
+    abstract void naam(String name);
+
+    abstract void about(String degree, int duration);
+}
---
+++ src/com/OOP/abstractDemo/Son.java
@@ -0,0 +1,20 @@
+package com.OOP.abstractDemo;
+
+public class Son extends Parent{
+
+    Son(String age) {
+        super(age);
+//        this.ageGroup = age;
+        System.out.println("AgeGroup: " + age);
+    }
+
+    @Override
+    void naam(String name) {
+        System.out.println("I am a Son, my name is " + name);
+    }
+
+    @Override
+    void about(String degree, int duration) {
+        System.out.println("My degree is " + degree + " duration is " + duration);
+    }
+}

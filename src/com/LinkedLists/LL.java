7f0b46ffc164dbb292fa42a2a45a203b60af4651
Ankit_Bharti <152873570+ankitbharti99@users.noreply.github.com>
2026-07-29 01:02:53 +0530

    linked list insertion

--- src/com/LinkedLists/LL.java
+++ src/com/LinkedLists/LL.java
@@ -9,6 +9,61 @@ public class LL {
         this.size = 0;
     }
 
+    public void insertAtFirst(int value) {
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+
+        if(tail == null) {
+            tail = head;
+        }
+
+        size += 1;
+    }
+
+    public void insertAtLast(int value) {
+        if(tail == null) {
+            insertAtFirst(value);
+            return;
+        }
+
+        Node node = new Node(value);
+        tail.next = node;
+        tail = node;
+
+
+        size += 1;
+    }
+
+    public void insertAtPosition(int value, int index) {
+        if(tail == null) {
+            insertAtFirst(value);
+            return;
+        }
+
+        int pos = index-1;
+        Node node = new Node(value);
+        node.pos =
+        head = node;
+
+        if(tail == null) {
+            tail = head;
+        }
+
+        size += 1;
+    }
+
+    public void display() {
+        Node temp = head;
+
+        while(temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("END");
+        System.out.println();
+    }
+
     private class Node {
         private int value;
         private Node next;
--- src/com/LinkedLists/Main.java
+++ src/com/LinkedLists/Main.java
@@ -3,5 +3,16 @@ package com.LinkedLists;
 public class Main {
     static void main() {
         LL list = new LL();
+        list.insertAtFirst(26);
+        list.insertAtFirst(6);
+        list.insertAtFirst(2);
+        list.insertAtFirst(15);
+        list.insertAtFirst(35);
+        list.display();
+
+        list.insertAtLast(25);
+        list.insertAtLast(13);
+        list.insertAtLast(5);
+        list.display();
     }
 }
